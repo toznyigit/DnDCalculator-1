aa24274d31da173a065057a670c5792af52235ce
Ahmet Burak Baraklı <ahmetburakbarakli@gmail.com>
2020-10-18 23:51:56 +0300

    Add new pots

--- DnD July/Potions.py
+++ DnD July/Potions.py
@@ -8,7 +8,9 @@ class Potion():
 potions = {
     "Charm":Potion("Charm",100,(2,"CHA"),2),
     "Battle":Potion("Battle",100,(2,"STR"),2),
+    "LesserHealth":Potion("Lesser Health",25,(),0),
     "Health":Potion("Health",50,(),0),
+    "GreaterHealth":Potion("Greater Health",100,(),0),
     "Invisibility":Potion("Invisibility",400,(0,0),3),
     "Resistance":Potion("Resistance",100,(),2),
     "Sense":Potion("Sense",200,(2,"WIS"),2),
