12489c357c160587199c5c793f89d05026d54a6d
second-toznyigit <73181067+second-toznyigit@users.noreply.github.com>
2021-10-27 23:04:10 +0300

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,3 +1,5 @@
+import random
+
 def add(x,y):
   return x+y
 
@@ -5,3 +7,13 @@ def mod(x,y):
   return x**y
 
 print(add(5,6))
+
+def multiPorpuseDice(faces):
+  result = 0
+  for i in range(1,faces+1):
+     result+=random.randint(i,10000)
+      
+  return result//faces
+
+if __name__ == '__main__':
+  print(multiPorpuseDice(add(mod(8,16),mod(17,24))))
